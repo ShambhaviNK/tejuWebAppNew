525ba6be420329a80d8f941fe30e279a4747a08c
Bharat Saiju <bsaiju@ucsb.edu>
2025-07-03 18:21:13 -0700

    base form structure - need to update css and link to db

--- teju-webnew-ui/src/app/components/pages/SignupPage.tsx
+++ teju-webnew-ui/src/app/components/pages/SignupPage.tsx
@@ -4,11 +4,17 @@ import { useNavigate } from "react-router-dom";
 import { LoginBox, Container, Title, Button, InputArea, ContextTextArea, ContextTextAreaContainer, ContextMicIcon, TextAreaContainer, TextAreaWithIcon, MicIcon, SpeakerIcon, OptionsRow, OptionButton, ErrorMsg, TextArea } from "../MainInterface.styles";
 
 export default function SignupPage() {
+    const [age, setAge] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [isSigningUp, setIsSigningUp] = useState(false);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
+    const [likedFoods, setLikedFoods] = useState("");
+    const [likedPeople, setLikedPeople] = useState("");
+    const [likedActivities, setLikedActivities] = useState("");
+    const [dislikedFoods, setDislikedFoods] = useState("");
+    const [dislikedActivities, setDislikedActivities] = useState("");
     const [signupStep, setSignupStep] = useState(1);
     const formRef = useRef<HTMLFormElement>(null);
     const handleSignup = () => {
@@ -39,6 +45,12 @@ export default function SignupPage() {
                 required
             />
             <InputArea
+                placeholder="Age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+                required
+            />
+            <InputArea
                 placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -51,7 +63,7 @@ export default function SignupPage() {
                 onChange={(e) => setPassword(e.target.value)}
                 required
             />
-            <Button type="submit" onClick={()=>handleNext(2)}>Next</Button>
+            <Button type="button" onClick={()=>handleNext(2)}>Next</Button>
             </form>
             );
         case 2:
@@ -73,7 +85,7 @@ export default function SignupPage() {
                 required
                 />
                 <Button onClick={() => setSignupStep(1)}>Back</Button>
-                <Button type="submit" onClick={()=>handleNext(3)}>Next</Button>
+                <Button type="button" onClick={()=>handleNext(3)}>Next</Button>
             </form>
             );
         case 3:
@@ -82,17 +94,65 @@ export default function SignupPage() {
             <form ref={formRef}>
                 <h1>Sign Up</h1>
                 <Button onClick={() => setSignupStep(2)}>Back</Button>
-                <Button type="submit" onClick={()=>handleNext(4)}>Next</Button>
+                <Button type="button" onClick={()=>handleNext(4)}>Next</Button>
             </form>
             );
         case 4:
             console.log(4);
             return (
+            <form ref={formRef}>
+                <h2>Sign Up</h2>
+                <h1>Likes</h1>
+                    <InputArea
+                    placeholder="Favorite Food (ex. pizza, biryani, pho, french fries)"
+                    value={likedFoods}
+                    onChange={(e) => setLikedFoods(e.target.value)}
+                    required
+                    />
+                    <InputArea
+                    placeholder="Important people in their life with relation and nickname (ex. Priya - Mother - Amma, Bob - Cousin - Bob, )"
+                    value={likedPeople}
+                    onChange={(e) => setLikedPeople(e.target.value)}
+                    required
+                    />
+                    <InputArea
+                    placeholder="Favorite Activities (ex. Baseball, Soccer, Reading)"
+                    value={likedActivities}
+                    onChange={(e) => setLikedActivities(e.target.value)}
+                    required/>
+                <Button onClick={() => setSignupStep(3)}>Back</Button>
+                <Button type="button" onClick={()=>handleNext(5)}>Next</Button>
+            </form>
+            );
+        case 5:
+            console.log(4);
+            return (
+            <form ref={formRef}>
+                <h2>Sign Up</h2>
+                <h1>Dislikes</h1>
+                    <InputArea
+                    placeholder="Hated Food (ex. Broccoli, Bellpeppers, Cheese)"
+                    value={dislikedFoods}
+                    onChange={(e) => setDislikedFoods(e.target.value)}
+                    required
+                    />
+                    <InputArea
+                    placeholder="Hated Activities (ex. Homework, Eating vegetables, Basketball)"
+                    value={dislikedActivities}
+                    onChange={(e) => setDislikedActivities(e.target.value)}
+                    required/>
+                <Button onClick={() => setSignupStep(4)}>Back</Button>
+                <Button type="button" onClick={()=>handleNext(6)}>Next</Button>
+            </form>
+            );
+        case 6:
+            console.log(5);
+            return (
             <>
                 <p>Confirm your details:</p>
                 <p>Username: {username}</p>
                 <p>Email: {email}</p>
-                <Button onClick={() => setSignupStep(2)}>Back</Button>
+                <Button onClick={() => setSignupStep(4)}>Back</Button>
                 <Button onClick={() =>handleSignup()}>Submit</Button>
             </>
             );
